8f43918f2e1e709a0bc3cf238a9e30bdabe1a12c
Andreza Feu <andrezafeu@gmail.com>
2016-06-29 00:34:52 -0400

    Disable submit button until passwords match criterias, show/hide messages on keyup, refactoring

--- js/app.js
+++ js/app.js
@@ -1,24 +1,52 @@
-var $password = $("#password")
-var $confirmPassword = $("#confirm_password")
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
 
 $("form span").hide();
 
+
+function isPasswordValid() {
+	return $password.val().length > 8;
+}
+
+
+function arePasswordsMatching() {
+	return $confirmPassword.val() === $password.val();
+}
+
+
+function canSubmit() {
+	return isPasswordValid() && arePasswordsMatching();
+}
+
+
 function passwordEvent() {
-	if ($password.val().length > 8) {
+	if (isPasswordValid()) {
 		$password.next().hide();
 	} else {
 		$password.next().show();
 	} 
 }
 
+
 function confirmPasswordEvent() {
-	if ($confirmPassword.val() === $("#password").val()) {
+	if (arePasswordsMatching()) {
 		$confirmPassword.next().hide();
 	} else {
 		$confirmPassword.next().show();
 	} 
 }
 
-$password.focus(passwordEvent);
 
-$confirmPassword.focus(confirmPasswordEvent);
+function enableSubmitEvent() {
+	// canSubmit is true so prop disabled would be true, therefore we need the ! in front of 
+	// canSubmit to flick the values
+	$("#submit").prop("disabled", !canSubmit());
+}
+
+
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+// It needs to be executed when the page loads
+enableSubmitEvent();
